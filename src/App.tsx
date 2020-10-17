87a4eb1a07344cf8971a88f154bc4cfa72f103eb
Hollerith98T <giuseppemazzara98@gmail.com>
2020-10-17 20:13:40 +0200

    Credits

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,17 @@ import './App.css';
 
 function App() {
   return (
-    <h1>Hello Worldddddd</h1>
-  );
+    <div className="App" >
+        <p>
+        The only video platform to review your DMI lessons online from the comfort of your home.
+        </p>
+    <p className="credits">
+      Coded by 
+      <a href="https://github.com/Hollerith98T">Giuseppe Mazzara</a>
+      <a href="https://github.com/Lorenzo-Pappalardo">Lorenzo Pappalardo</a>
+    </p>
+    </div>
+  );  
 }
 
 export default App;
