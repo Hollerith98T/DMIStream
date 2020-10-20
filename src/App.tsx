66d4a1e8d1641f0073ca985a1c24531bb3b48ce0
Hollerith98T <giuseppemazzara98@gmail.com>
2020-10-20 13:12:51 +0200

    CSS, APP and Other Problems

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,62 @@
 import React from 'react';
 import './App.css';
+import { subjects1, subjects2, subjects3 } from './groups';
 
 function App() {
-  let subjects: string[] = [];
-
-  for (let i: number = 0; i < 10; i++) {
-    subjects.push(`Materia N.${i}`);
-  }
-
   return (
     <div>
       <h1 className="title">DMIStream</h1>
       <p className="description">
-      Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-      Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
+        been the industry's standard dummy text ever since the 1500s.
       </p>
       <div className="contents-grid">
         <div className="columns1">
-          {subjects.map((subject, index) => (
-            <p>{subject}</p>
-          ))}
+          <div className="first-semester">
+            <h2> Primo Anno</h2>
+            <h4> Primo semestre </h4>
+            {subjects1.map((subject, index) => (
+              <p>{subject}</p>
+            ))}
+          </div>
+          <div className="second-semester">
+            <h4> Secondo semestre </h4>
+            {subjects1.map((subject, index) => (
+              <p>{subject}</p>
+            ))}
+          </div>
         </div>
+
         <div className="columns2">
-          {subjects.map((subject, index) => (
-            <p>{subject}</p>
-          ))}
+          <div className="first-semester">
+          <h2> Secondo Anno</h2>
+            <h4> Primo semestre </h4>
+            {subjects2.map((subject, index) => (
+              <p>{subject}</p>
+            ))}
+          </div>
+          <div className="second-semester">
+            <h4> Secondo semestre </h4>
+            {subjects2.map((subject, index) => (
+              <p>{subject}</p>
+            ))}
+          </div>
         </div>
+
         <div className="columns3">
-          {subjects.map((subject, index) => (
-            <p>{subject}</p>
-          ))}
+          <div className="first-semester">
+          <h2> Terzo Anno</h2>
+            <h4> Primo semestre </h4>
+            {subjects3.map((subject, index) => (
+              <p>{subject}</p>
+            ))}
+          </div>
+          <div className="second-semester">
+            <h4> Secondo semestre </h4>
+            {subjects3.map((subject, index) => (
+              <p>{subject}</p>
+            ))}
+          </div>
         </div>
       </div>
     </div>
