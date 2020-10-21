b8d764cf34f177d8bed9811657af19079becc2e6
Lorenzo Pappalardo <lorenzopappalardo7@gmail.com>
2020-10-21 17:57:46 +0200

    Added SubjectPage to routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import './App.css';
-import { Homepage } from './Homepage';
 import { HashRouter, Route, Redirect } from 'react-router-dom';
+import { Homepage } from './Homepage';
+import { Subject } from './SubjectPage';
 
 function App(): JSX.Element {
   return (
     <HashRouter>
       <Route exact path="/homepage">
         <Homepage />
+      </Route>
+      <Route exact path="/subjectpage">
+        <Subject />
       </Route>
       <Redirect to="/homepage/" />
     </HashRouter>
