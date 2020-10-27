3ea5212bea278493ff6938cf00687de8c7a8f5ec
Lorenzo Pappalardo <lorenzopappalardo7@gmail.com>
2020-10-27 20:30:04 +0100

    Updates

--- README.md
+++
@@ -1,4 +0,0 @@
-# DMIStream
-The only video platform to review your DMI lessons online from the comfort of your home.
-
-## Live [here](https://hollerith98t.github.io/DMIStream/)
---
+++ precache-manifest.d9fc64d10684ec55693cfa5691b18284.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83df37e08d7451f2b76cfd7828a8cf23",
+    "url": "./index.html"
+  },
+  {
+    "revision": "db2f6013f9d8fc77ecb1",
+    "url": "./static/css/main.627573fc.chunk.css"
+  },
+  {
+    "revision": "3e1f49e896a7ba85c022",
+    "url": "./static/js/2.2df2cc9d.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "./static/js/2.2df2cc9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db2f6013f9d8fc77ecb1",
+    "url": "./static/js/main.3ab92cc4.chunk.js"
+  },
+  {
+    "revision": "60766f91cac8fef62b25",
+    "url": "./static/js/runtime-main.291b38df.js"
+  }
+]);
